5a499ebac46422968c9831a3461f3fe8d5ab04f3
Alan Gibson <alan.gibson@gmail.com>
2026-06-21 14:43:02 +0200

    Switching to pulled up switches

--- test/test_arc_ok.sh
+++ test/test_arc_ok.sh
@@ -6,9 +6,9 @@ PIN="${1:-26}"
 STATE="$(pinctrl get "${PIN}")"
 
 if [[ "${STATE}" == *" hi "* ]]; then
-    echo "1: Arc is on"
-elif [[ "${STATE}" == *" lo "* ]]; then
     echo "0: Arc is off"
+elif [[ "${STATE}" == *" lo "* ]]; then
+    echo "1: Arc is on"
 else
     echo "Unable to read GPIO ${PIN} state: ${STATE}" >&2
     exit 1
